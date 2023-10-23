9ed549bdac61f076d8b7200bd42bd2d553132c53
OlgaGabbert <olga.gabbert@gmail.com>
2023-10-23 10:57:13 +0200

    add styles for project cards

--- script.js
+++ script.js
@@ -53,54 +53,88 @@ class MyProjects extends HTMLElement {
                 display: flex; 
                 justify-content: center; 
                 align-items: center; 
-                gap: 100px;
                 flex-wrap: wrap;
                 max-width: 1000px;
-                mid-width: 500px;
+                gap: 50px;
                 margin-bottom: 20px; 
             }
 
-            a {
-                text-decoration: none;
-                color: #1e5f95;
+            .project-card {
+              border: 1px solid #ddd;
+              border-radius: 5px;
+              box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+              padding: 20px;
+              text-align: center;
+              background-color: #fff;
+              margin: 20px;
+              max-width: 200px;
+              transition: transform 0.3s;
+            }
+            
+            .project-card img {
+              max-width: 100%;
+              height: auto;
+              border-radius: 5px;
+            }
+            
+            .project-card h3 {
+              font-size: 1.5rem;
+              margin: 10px 0;
+              color: #333;
+            }
+            
+            .project-card p {
+              font-size: 1rem;
+              margin: 10px 0;
+              color: #666;
             }
 
-            a:hover {
-                font-weight: bold;
+            .project-card:hover {
+              transform: scale(1.05);
             }
+            
+            .project-card a {
+              transition: transform 0.3s;
+              display: inline-block;
+              padding: 10px 20px;
+              border: 1px solid #3498db; 
+              border-radius: 5px;
+              transition: background-color 0.3s, border-color 0.3s;
+              color: #1e5f95;
+              background: linear-gradient(135deg, #ffffff, #3498db);
+              text-decoration: none;
+              margin-top: 10px;
+              transition: background-color 0.3s;
+            }
+            
+            .project-card a:hover {
+              background: linear-gradient(135deg, #ffffff, hsl(209, 75%, 30%));
+              border-color: #1e5f95;
+              transform: scale(1.05);
+            }
+            
 
             </style>
-        <div class="project">
-        <h2>Project 1: Project Name</h2>
+
+        <div class="project-card">
+        <img src="project-image1.jpg" alt="Project Name">
+        <h3>Project 1: Project Name</h3>
         <p>Description of Project 1.</p>
-        <a href="project1.html">View Project</a>
+        <a href="project1.html">Learn more</a>
       </div>
     
-      <div class="project">
-        <h2>Project 2: Project Name</h2>
+      <div class="project-card">
+        <img src="project-image2.jpg" alt="Project Name">
+        <h3>Project 2: Project Name</h3>
         <p>Description of Project 2.</p>
-        <a href="project2.html">View Project</a>
+        <a href="project2.html">Learn more</a>
       </div>
-      <div class="project">
-        <h2>Project 3: Project Name</h2>
-        <p>Description of Project 3.</p>
-        <a href="project3.html">View Project</a>
-      </div>
-      <div class="project">
-        <h2>Project 4: Project Name</h2>
-        <p>Description of Project 4.</p>
-        <a href="project4.html">View Project</a>
-      </div>
-      <div class="project">
-        <h2>Project 5: Project Name</h2>
-        <p>Description of Project 5.</p>
-        <a href="project5.html">View Project</a>
-      </div>
-      <div class="project">
-        <h2>Project 6: Project Name</h2>
-        <p>Description of Project 6.</p>
-        <a href="project6.html">View Project</a>
-      </div>
+      <div class="project-card">
+      <img src="project-image3.jpg" alt="Project Name">
+      <h3>Project 3: Project Name</h3>
+      <p>Description of Project 3.</p>
+      <a href="project3.html">Learn more</a>
+    </div>
         `;
         shadowRoot.appendChild(myProjects);
     }
